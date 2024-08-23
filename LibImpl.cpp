8671f2601ae26b0252e312eeba56d5c8170c7001
Mzying2001 <41951400+Mzying2001@users.noreply.github.com>
2024-08-23 23:42:43 +0800

    miniz

--- LibImpl.cpp
+++ LibImpl.cpp
@@ -1,7 +1,12 @@
 ﻿#include "pch.h"
 
+// stb_image
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb/stb_image.h"
 
+// stb_image_write
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb/stb_image_write.h"
+
+// miniz
+#include "miniz/miniz.c"
